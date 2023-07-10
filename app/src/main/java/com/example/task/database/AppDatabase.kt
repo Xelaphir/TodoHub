63f689d223f0c76c6269c995197dbca537ed5ff1
Zefeerka <alex.lugovoy88@gmail.com>
2023-07-10 17:57:41 +1000

    Update database

--- app/src/main/java/com/example/task/database/AppDatabase.kt
+++ app/src/main/java/com/example/task/database/AppDatabase.kt
@@ -3,9 +3,11 @@ package com.example.task.database
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import com.example.task.data.group.Group
+import com.example.task.data.task.Task
 
-@Database(entities = [Group::class], version = 1)
+@Database(entities = [Group::class, Task::class], version = 1)
 abstract class AppDatabase : RoomDatabase() {
 
     abstract fun groupDao(): GroupDao
+    abstract fun taskDao(): TaskDao
 }
