ade0f48372aaa1c14732c01da274232e58adfb42
Zefeerka <alex.lugovoy88@gmail.com>
2023-07-10 14:00:04 +1000

    Add create task dialog on MainFragment

--- app/src/main/java/com/example/task/ui/main/MainFragment.kt
+++ app/src/main/java/com/example/task/ui/main/MainFragment.kt
@@ -1,18 +1,24 @@
 package com.example.task.ui.main
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.WindowManager
 import android.widget.Toast
 import androidx.appcompat.widget.PopupMenu
 import androidx.fragment.app.Fragment
 import com.example.task.R
+import com.example.task.databinding.CreateTaskDialogBinding
 import com.example.task.databinding.FragmentMainBinding
+import com.google.android.material.bottomsheet.BottomSheetDialog
 
 class MainFragment : Fragment() {
 
     private lateinit var binding: FragmentMainBinding
+    private lateinit var createTaskDialog: BottomSheetDialog
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -23,6 +29,7 @@ class MainFragment : Fragment() {
         binding = FragmentMainBinding.inflate(inflater, container, false)
 
         initCreateFab()
+        initCreateTaskDialog()
 
         return binding.root
 
@@ -35,7 +42,7 @@ class MainFragment : Fragment() {
         popupMenu.setOnMenuItemClickListener() {
             when(it.itemId) {
                 R.id.create_task -> {
-                    Toast.makeText(requireContext(), "Create task", Toast.LENGTH_SHORT).show()
+                    createTaskDialog.show()
                     true
                 }
                 R.id.create_group -> {
@@ -50,6 +57,51 @@ class MainFragment : Fragment() {
             popupMenu.show()
         }
 
+    }
+
+    private fun initCreateTaskDialog() {
+
+        createTaskDialog = BottomSheetDialog(requireContext(), R.style.DialogStyle)
+        createTaskDialog.window?.addFlags(WindowManager.LayoutParams.FLAG_DIM_BEHIND)
+
+        val dialogBinding = CreateTaskDialogBinding.inflate(LayoutInflater.from(requireContext()), null, false)
+
+        dialogBinding.saveTaskButton.isEnabled = false
+
+        dialogBinding.descriptionImageButton.setOnClickListener {
+            dialogBinding.descriptionEditText.visibility = View.VISIBLE
+        }
+        dialogBinding.addToFavoriteCheckBox.setOnCheckedChangeListener { _, isChecked ->
+            dialogBinding.addToFavoriteCheckBox.setButtonDrawable(
+                if (isChecked) R.drawable.icon_star else R.drawable.icon_star_border
+            )
+        }
+
+        dialogBinding.titleEditText.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+
+            override fun afterTextChanged(s: Editable?) {
+                dialogBinding.saveTaskButton.isEnabled = s?.isNotBlank() == true
+            }
+        })
+
+        dialogBinding.saveTaskButton.setOnClickListener {
+            Toast.makeText(requireContext(), "Create", Toast.LENGTH_SHORT).show()
+            createTaskDialog.dismiss()
+        }
+        dialogBinding.titleEditText.requestFocus()
+
+        createTaskDialog.setOnDismissListener {
+            dialogBinding.apply {
+                titleEditText.text.clear()
+                descriptionEditText.text.clear()
+                descriptionEditText.visibility = View.GONE
+                addToFavoriteCheckBox.isChecked = false
+            }
+        }
+
+        createTaskDialog.setContentView(dialogBinding.root)
     }
 
 }
