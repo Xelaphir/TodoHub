3d99cc5618796f7af25bd2788af16cac5348ae51
Zefeerka <alex.lugovoy88@gmail.com>
2023-07-10 13:13:48 +1000

    Add main fragment layout

--- app/src/main/java/com/example/task/MainActivity.kt
+++ app/src/main/java/com/example/task/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.task
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.task.databinding.ActivityMainBinding
+import com.example.task.ui.main.MainFragment
 
 class MainActivity : AppCompatActivity() {
 
@@ -10,6 +11,12 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater).also { setContentView(it.root) }
+
+        if(savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .add(R.id.fragment_holder, MainFragment())
+                .commit()
+        }
 
     }
 }
