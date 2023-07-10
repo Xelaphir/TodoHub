5c6385cd8f594a15919d246fbc367c232a6f24b6
Zefeerka <alex.lugovoy88@gmail.com>
2023-07-10 21:17:51 +1000

    Add navigator implementation

--- app/src/main/java/com/example/task/MainActivity.kt
+++ app/src/main/java/com/example/task/MainActivity.kt
@@ -2,10 +2,12 @@ package com.example.task
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import com.example.task.databinding.ActivityMainBinding
 import com.example.task.ui.main.MainFragment
+import com.example.task.ui.navigator.Navigator
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), Navigator {
 
     private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,10 +15,25 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater).also { setContentView(it.root) }
 
         if(savedInstanceState == null) {
-            supportFragmentManager.beginTransaction()
-                .add(R.id.fragment_holder, MainFragment())
-                .commit()
+            launchFragment(MainFragment(), false)
         }
 
+    }
+
+    private fun launchFragment(fragment: Fragment, addToBackStack: Boolean = true) {
+        val transaction = supportFragmentManager.beginTransaction()
+        if (addToBackStack) transaction.addToBackStack(null)
+        transaction
+            .replace(R.id.fragment_holder, fragment)
+            .commit()
+    }
+
+    override fun launch(fragment: Fragment) {
+        launchFragment(fragment)
+    }
+
+    @Suppress("DEPRECATION")
+    override fun goBack() {
+        onBackPressed()
     }
 }
