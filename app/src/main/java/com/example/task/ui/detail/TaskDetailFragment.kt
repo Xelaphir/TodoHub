c0ad9f1470d4225f7d3afc864877d27eef664944
Zefeerka <alex.lugovoy88@gmail.com>
2023-07-10 21:23:23 +1000

    Add navigator on MainFragment

--- app/src/main/java/com/example/task/ui/detail/TaskDetailFragment.kt
+++ app/src/main/java/com/example/task/ui/detail/TaskDetailFragment.kt
@@ -1,4 +1,39 @@
 package com.example.task.ui.detail
 
-class TaskDetailFragment {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.core.os.bundleOf
+import androidx.fragment.app.Fragment
+import com.example.task.databinding.FragmentTaskDetailBinding
+import java.util.UUID
+
+class TaskDetailFragment : Fragment() {
+
+    private lateinit var binding: FragmentTaskDetailBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentTaskDetailBinding.inflate(inflater, container, false)
+
+        return binding.root
+    }
+
+    companion object {
+
+        private const val ARGS = "args"
+
+        fun newInstance(id: UUID): TaskDetailFragment {
+            val arguments = bundleOf(ARGS to id)
+            val f = TaskDetailFragment()
+            f.arguments = arguments
+
+            return f
+        }
+    }
+
 }
