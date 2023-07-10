11d9afd91fb041ce72f0734b250a00107a720a2e
Zefeerka <alex.lugovoy88@gmail.com>
2023-07-10 17:56:44 +1000

    Add task model

---
+++ app/src/main/java/com/example/task/data/task/Task.kt
@@ -0,0 +1,16 @@
+package com.example.task.data.task
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import java.util.UUID
+
+@Entity
+data class Task(
+    @PrimaryKey val id: UUID = UUID.randomUUID(),
+    var title: String,
+    var description: String,
+    var isCompleted: Boolean,
+    var isFavorite: Boolean,
+    var parent: UUID?,
+    var group: String,
+)
