5b3176b0f70c688be99c0f22e50201a34dc8a42e
Zefeerka <alex.lugovoy88@gmail.com>
2023-07-10 16:01:05 +1000

    Init database in app class

--- app/src/main/java/com/example/task/App.kt
+++ app/src/main/java/com/example/task/App.kt
@@ -1,7 +1,13 @@
 package com.example.task
 
 import android.app.Application
+import com.example.task.database.DatabaseBuilder
 
 class App : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        DatabaseBuilder.build(this)
+    }
 
 }
