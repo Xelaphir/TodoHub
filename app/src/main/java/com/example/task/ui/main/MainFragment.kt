0c5ca0b37bdb7133721758b0b982c59ebb6e6126
Zefeerka <alex.lugovoy88@gmail.com>
2023-07-10 13:47:29 +1000

    Add popupmenu for creating fab

--- app/src/main/java/com/example/task/ui/main/MainFragment.kt
+++ app/src/main/java/com/example/task/ui/main/MainFragment.kt
@@ -4,7 +4,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.appcompat.widget.PopupMenu
 import androidx.fragment.app.Fragment
+import com.example.task.R
 import com.example.task.databinding.FragmentMainBinding
 
 class MainFragment : Fragment() {
@@ -19,7 +22,33 @@ class MainFragment : Fragment() {
 
         binding = FragmentMainBinding.inflate(inflater, container, false)
 
+        initCreateFab()
+
         return binding.root
+
+    }
+
+    private fun initCreateFab() {
+        val popupMenu = PopupMenu(requireContext(), binding.createItemFab)
+        popupMenu.inflate(R.menu.create_item_menu)
+
+        popupMenu.setOnMenuItemClickListener() {
+            when(it.itemId) {
+                R.id.create_task -> {
+                    Toast.makeText(requireContext(), "Create task", Toast.LENGTH_SHORT).show()
+                    true
+                }
+                R.id.create_group -> {
+                    Toast.makeText(requireContext(), "Create task", Toast.LENGTH_SHORT).show()
+                    true
+                }
+                else -> false
+            }
+        }
+
+        binding.createItemFab.setOnClickListener {
+            popupMenu.show()
+        }
 
     }
 
